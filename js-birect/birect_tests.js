fec36c27d65d58893541baa269a7b9b6b113b2dc
Marcus Westin <marcus.westin@gmail.com>
2016-01-31 12:44:07 -0500

    Use tinytest 0.0.2

--- js-birect/birect_tests.js
+++ js-birect/birect_tests.js
@@ -1,20 +1,14 @@
-if (typeof(process) !== 'undefined' && process.title === 'node') {
-	// avoid browserify bundling with toString()
-	global.async = require('asyncawait/async'.toString())
-	global.await = require('asyncawait/await'.toString())
-}
-
-var {test, check, checkErr, print} = require('tinytest')
+var {test, check, assert, checkErr, print, await} = require('tinytest')
 var conn = require('./birect-conn')
 var server = require('./birect-server')
 var client = require('./birect-client')
 
 var port = 8085
 
-test('Start server 1', function(done) {
+test('Start server 1', function() {
 	var http = require('http')
 	var httpServer = http.createServer((req, res) => { res.end('Hi') })
-	await (new Promise((resolve, reject) => {
+	await(new Promise((resolve, reject) => {
 		httpServer.listen(port, () => {
 			console.log("Server listening on", port)
 			resolve()
@@ -30,12 +24,12 @@ test('Start server 1', function(done) {
 })
 
 test('Echo client', function() {
-	var c = await (client.connect("ws://localhost:"+port+"/birect/upgrade"))
+	var c = await(client.connect("ws://localhost:"+port+"/birect/upgrade"))
 	var res = await(c.sendJSONReq("Echo", { Text:"Foo" }))
 	assert(res.Text == 'Foo')
 })
 
-test('Start server 2', function(done) {
+test('Start server 2', function() {
 	var birectServer = server.listenAndServe(port + 1)
 	birectServer.handleJSONReq('Echo', function(req) {
 		return new Promise((resolve) => {
@@ -45,15 +39,7 @@ test('Start server 2', function(done) {
 })
 
 test('Echo client 2', function() {
-	var c = await (client.connect("ws://localhost:"+(port + 1)+"/birect/upgrade"))
+	var c = await(client.connect("ws://localhost:"+(port + 1)+"/birect/upgrade"))
 	var res = await(c.sendJSONReq("Echo", { Text:"Foo" }))
 	assert(res.Text == 'Foo')
 })
-
-// Util
-///////
-
-function assert(ok, msg) {
-	if (ok) { return }
-	throw new Error('assert failed' + (msg ? ': ' + msg : ''))
-}
